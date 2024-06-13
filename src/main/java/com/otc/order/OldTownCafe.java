9d8fc6564e1499c5704f81f33f7d2baff6aafaaa
mmsutradhar <mmsutradhar@gmail.com>
2024-06-13 05:56:16 +0530

    Initial Commit

---
+++ src/main/java/com/otc/order/OldTownCafe.java
@@ -0,0 +1,15 @@
+package com.otc.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldTownCafe {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldTownCafe.class, args);
+		
+		System.out.println("Old Town Cafe Application Has Started");
+	}
+
+}
